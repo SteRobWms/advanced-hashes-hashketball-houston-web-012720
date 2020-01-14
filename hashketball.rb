559616a6d5d9aa05f63c5a5bb7baeaa1f7972037
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-14 21:13:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,7 +206,7 @@ def winning_team()
   scores_by_name = {}
   game_hash.each_key do |key|
     scores_by_name[key] = {}
-    scores_by_name[key][:team_name] => 0
+    scores_by_name[key][:team_name] = 0
     game_hash[key][:players].each do |i|
       scores_by_name[key][:team_name] += i[:points]
     end

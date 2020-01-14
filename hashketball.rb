5e0aa8171c7116f399a1c23e7ab2a21cfc64c6a9
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-14 20:29:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ def player_stats(player_name)
   game_hash.each_key do |key|
     # game_hash[key][:players].each do |i|
     #   if i[:player_name] == player_name
-    #     i.each{|k, l| stats[:player_name][k] = l}
+    #     i.each{|k, l| stats[k] = l}
     #   end
     # end
   end

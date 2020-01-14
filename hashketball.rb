c35bf71086c7d4dac1ec7c3aff2b05ea93e494f8
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-14 18:53:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,7 +116,7 @@ def num_points_scored(player_name)
   game_hash.each_key do |key|
     game_hash[key][:players].each do |i|
       if i[:player_name] == (player_name)
-        p i[:number]
+        return i[:points]
       end
     end
   end
